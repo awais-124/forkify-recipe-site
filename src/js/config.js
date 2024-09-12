727982e1d381e2d664324084469b68b513d2d2a1
awais-124 <awais14940@gmail.com>
2024-09-12 15:12:00 +0500

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ad951202-6560-409d-a1cd-5f102d130c1b';
+export const MODAL_CLOSE_SEC = 2.5;
